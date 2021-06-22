2bc0df31c4934524be57e1fd943556f7e797514c
kubot080301 <kubot100601@gmail.com>
2021-06-22 10:41:21 +0800

    update

--- kubot_bringup/include/kubot_bringup/data_holder.h
+++ kubot_bringup/include/kubot_bringup/data_holder.h
@@ -9,13 +9,7 @@ using namespace std;
 
 typedef int int32;
 typedef short int16;
-typedef unsigned short uint16;
-
-struct Robot_firmware
-{
-   char version[16]; //控制板版本
-   char time[16];    //建構時間
-};
+typedef unsigned short unint16;
 
 enum IMU_TYPE
 {
@@ -30,59 +24,68 @@ enum MODEL_TYPE
 {
    MODEL_TYPE_2WD_DIFF = 1,
    MODEL_TYPE_4WD_DIFF = 2,
-   MODEL_TYPE_3WD_OMNI = 101,
-   MODEL_TYPE_4WD_OMNI = 102,
-   MODEL_TYPE_4WD_MECANUM = 201,
+   MODEL_TYPE_3WD_OMNI = 11,
+   MODEL_TYPE_4WD_OMNI = 12,
+   MODEL_TYPE_4WD_MECANUM = 21,
+};
+
+struct Robot_firmware
+{
+   char version[16];   // Driver board version.
+   char time[16];   // Build time.
 };
 
 struct Robot_parameter
 {
    union
    {
-      char buff[64]; //總長度64位元
+      char buff[64];   // Total length is 64 bits.
       struct
       {
-         unsigned short wheel_diameter;     // 輪子直徑(mm)
-         unsigned short wheel_track;        //差分:兩輪中心距、三輪全向:輪心直徑、四輪全向:前後輪中心距+左右輪中心距 (mm)
-         unsigned short encoder_resolution; // 編碼器解析度
-         unsigned char do_pid_interval;     // PID間隔 (ms)
+         unsigned short wheel_diameter;	// Wheel diameter (mm).
+         unsigned short wheel_track;	// Wheel track (mm).
+         unsigned short encoder_resolution;	// Encoder resolution.
+         unsigned char do_pid_interval;	// PID interval (ms).
          unsigned short kp;
          unsigned short ki;
          unsigned short kd;
-         unsigned short ko;              // PID比例
-         unsigned short cmd_last_time;   // 命令維持時間(ms)，超過該時間會自動停止運動
-         unsigned short max_v_liner_x;   // 最大X方向線速度
-         unsigned short max_v_liner_y;   // 最大Y方向線速度 (差分輪為0)
-         unsigned short max_v_angular_z; // 最大旋轉速度
-         unsigned char imu_type;         // IMU類型
-         unsigned short motor_ratio;     // 馬達減速比
-         unsigned char model_type;       // 運動模型類型
-         unsigned short sona_distance;   // 超音波感測距離
-         };
-    };
+         unsigned short ko;
+         unsigned short cmd_last_time;   // After this time, cmd_vel will automatically stop moving.
+         unsigned short max_v_liner_x;   // Maximum linear velocity in X direction (cm/s). 
+         unsigned short max_v_liner_y;   // Maximum linear velocity in Y direction (cm/s) (DIFF model is 0). 
+         unsigned short max_v_angular_z;   // Maximum rotation velocity (0.01 rad/s).
+         unsigned char imu_type;
+         unsigned short motor_ratio;	// Motor reduction ratio.
+         unsigned char model_type;   // Motion model type.
+         unsigned short sona_distance;   // Ultrasonic induction deceleration distance.
+      };
+   };
 };
 
 struct Robot_velocity
 {
-   short v_liner_x;   // 線速度       前>0 cm/s
-   short v_liner_y;   // 差分輪為0  cm/s      全向左為>0
-   short v_angular_z; // 角速度 左>0 0.01rad/s  100 means 1 rad/s
+   short v_liner_x;  // Linear velocity in X direction. (cm/s)
+   short v_liner_y;   // Linear velocity in Y direction(cm/s) (DIFF model is 0). 
+   short v_angular_z;   // Angular velocity (0.01 rad/s).
 };
 
-struct Robot_odom
+struct Robot_odom   // Odometer calculated by Driver Board.
 {
-   short v_liner_x;   // 線速度 前>0 後<0  cm/s
-   short v_liner_y;   // 差分輪 为0        cm/s
-   short v_angular_z; // 角速度 左>0 右<0  0.01rad/s   100 means 1 rad/s
-   int32 x;           // 里程計座標x       cm (long 4個字節)
-   int32 y;           // 里程計座標y       cm (long 4個字節)
-   short yaw;         // 里程計航角        0.01rad     100 means 1 rad
+   short v_liner_x;	//	Forward is > 0, Back is < 0, cm/s.
+   short v_liner_y;	// Left is > 0, Right is < 0, cm/s.
+   short v_angular_z;	// Left is > 0, Right is < 0, cm/s.
+   int32 x;   // Odometer X coordinate (cm).
+   int32 y;   // Odometer Y coordinate (cm).
+   int32 z;   // Odometer Z coordinate (cm).
+   short roll;   // Odometer Roll coordinate (0.01 rad).
+   short pitch;   // Odometer Pitch coordinate (0.01 rad).
+   short yaw;   // Odometer Yaw coordinate (0.01 rad).
 };
 
 struct Robot_pid_data
 {
-   int32 input[4];  //各輪子输入值
-   int32 output[4]; //各輪子输出值
+   int32 input[4];	// The input response of each motor.
+   int32 output[4];	// The output response of each motor.
 };
 
 struct Robot_imu
@@ -92,34 +95,40 @@ struct Robot_imu
       float imu_data[9];
       struct
       {
-         float ax; // 加速度x m/s^2
-         float ay; // 加速度y m/s^2
-         float az; // 加速度z m/s^2
-         float gx; // 角速度x rad/s^2
-         float gy; // 角速度y rad/s^2
-         float gz; // 角速度z rad/s^2
-         float mx; // 磁力x mGA
-         float my; // 磁力y mGA
-         float mz; // 磁力z mGA
+         float ax;   // X linear acceleration (m/s^2).
+         float ay;   // Y linear acceleration (m/s^2).
+         float az;   // Z linear acceleration (m/s^2).
+         float gx;   // X angular rate (rad/s^2).
+         float gy;   // Y angular rate (rad/s^2).
+         float gz;   // Z angular rate (rad/s^2).
+         float mx;   // X gauss magnetic (mGA).
+         float my;   // Y gauss magnetic (mGA).
+         float mz;   // Z gauss magnetic (mGA).
       } imu;
    };
 };
 
 struct Robot_status
 {
-   bool bumper_status;         //防撞條狀態
-   unsigned char sona_data[8]; //sona raw_data
+   bool bumper_status;	// Robot bumper status (true is collision!)
+   unsigned char sona_data[8];   // The sensor returns data of each ultrasonic (mm). 
 };
 
 struct Robot_lcd_status
 {
-   //    char name[16];         //機器人名字
-   char robot_ip[16]; //設定顯示於下位機之機器人ip
+   //	char robot_name;
+   char robot_ip[16];	// ROS IP. It will be displayed on the LCD panel on the robot.
 };
 
 struct Robot_mcu_voltage
 {
-   unsigned short mcu_voltage; // 下位機電壓
+   unsigned short mcu_voltage;	// Driver board voltage (V).
+};
+
+struct RObot_status
+{
+   struct Robot_bumper;
+   struct Robot_sona_sensor;
 };
 
 #pragma pack(0)
@@ -127,17 +136,16 @@ struct Robot_mcu_voltage
 class Data_holder
 {
 public:
-   static Data_holder *get()
+   static Data_holder* get()
    {
       static Data_holder dh;
       return &dh;
    }
 
    void load_parameter();
-
    void save_parameter();
 
-   static void dump_params(struct Robot_parameter *params);
+   static void dump_params(struct Robot_parameter* params);
 
 private:
    Data_holder()
@@ -147,10 +155,10 @@ private:
       memset(&velocity, 0, sizeof(struct Robot_velocity));
       memset(&odom, 0, sizeof(struct Robot_odom));
       memset(&pid_data, 0, sizeof(struct Robot_pid_data));
+      memset(&imu_data, 0, sizeof(imu_data));
       memset(&robot_status, 0, sizeof(struct Robot_status));
       memset(&lcd_status, 0, sizeof(struct Robot_lcd_status));
       memset(&mcu_voltage, 0, sizeof(struct Robot_mcu_voltage));
-      memset(&imu_data, 0, sizeof(imu_data));
    }
 
 public:
@@ -159,11 +167,10 @@ public:
    struct Robot_velocity velocity;
    struct Robot_odom odom;
    struct Robot_pid_data pid_data;
+   float imu_data[9];
    struct Robot_status robot_status;
    struct Robot_lcd_status lcd_status;
    struct Robot_mcu_voltage mcu_voltage;
-
-   float imu_data[9];
 };
 
-#endif  /*  KUBOT_DATA_HOLDER_H_  */
+#endif /* KUBOT_DATA_HOLDER_H_ */
